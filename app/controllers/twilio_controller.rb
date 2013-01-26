6a15e3098178d132cb5861415fd25341485058c8
nsbarrcanvas <nick@canv.as>
2013-01-26 16:49:08 -0500

    trying to add logic to response

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,7 +2,7 @@ class TwilioController < ApplicationController
 
   def process_sms
     @rating = params[:body]
-    if @rating == 10
+    if @rating == "10"
       render 'process_sms.xml.erb', :content_type => 'text/xml'
     end
   end
