416d43b6b4db9a274de023da62af0999ec417d01
nsbarrcanvas <nick@canv.as>
2013-01-26 16:44:32 -0500

    trying to add logic to response

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,9 +1,10 @@
 class TwilioController < ApplicationController
 
   def process_sms
-    @city = params[:FromCity].capitalize
-    @state = params[:FromState]
-    render 'process_sms.xml.erb', :content_type => 'text/xml'
+    @rating = params[:body]
+    if @rating == 10
+      render 'process_sms.xml.erb', :content_type => 'text/xml'
+    end
   end
 
 end
