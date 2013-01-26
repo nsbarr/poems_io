781a1e250f6dfdf4af6935d7ba4718af1903fda9
nsbarrcanvas <nick@canv.as>
2013-01-26 17:16:45 -0500

    gettin cocky

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,12 +2,11 @@ class TwilioController < ApplicationController
 
   def process_sms
     @rating = params[:Body]
-    #if @rating == "10"
-    #  render 'process_sms.xml.erb', :content_type => 'text/xml'
-    #else
-    #  render 'process_sms_fail.xml.erb', :content_type => 'text/xml'
-    #end
-    render 'process_sms.xml.erb', :content_type => 'text/xml'
+    if @rating == "10"
+      render 'process_sms.xml.erb', :content_type => 'text/xml'
+    else
+      render 'process_sms_fail.xml.erb', :content_type => 'text/xml'
+    end
   end
 
 end
